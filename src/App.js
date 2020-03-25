a07e57772e857686b48752e75816256fa6aa9338
lupetyman <lupetyman@gmail.com>
2020-03-25 18:55:56 -0400

    add registration functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+
+class App extends Component {
+
+  state = {
+    isLoggedIn: false,
+    user: {}
+  };
+
+  //Check if user is logged in
+  componentDidMount() {
+    this.loginStatus()
+  };
+
+  //Fetch login status from Rails server.
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', {withCredentials: true})
+      .then(response => {
+        if (response.data.logged_in) {
+          this.handleLogin(response)
+        } else {
+          this.handleLogout()
+        }
+      })
+      .catch(error => console.log('API error:', error))
+  };
+
+  // Handle user log in and out.
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  };
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  };
+
+  render() {
+    return (
+      <div>
+      <BrowserRouter>
+        <Switch>
+
+          <Route exact path='/'
+          render={() => <Home
+          loggedInStatus={this.state.isLoggedIn}/>}
+          />
+
+          <Route exact path='/login'
+          render={() => <Login
+          handleLogin={this.handleLogin}
+          loggedInStatus={this.state.isLoggedIn}/>}
+          />
+
+          <Route exact path='/signup'
+          render={() => <Signup
+          handleLogin={this.handleLogin}
+          loggedInStatus={this.state.isLoggedIn}/>}
+          />
+
+        </Switch>
+      </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
